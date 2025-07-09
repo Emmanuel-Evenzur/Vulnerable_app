8fa7f7d7bb4279a1262d341172c63671bdbb471b
Emmanuel-Evenzur <emmanuel.evenzur@mend.io>
2025-07-09 17:06:28 +0300

    Update MyEntitiesRepository.java

--- src/main/java/com/afs/vulnerable/springbreak/MyEntitiesRepository.java
+++ src/main/java/com/afs/vulnerable/springbreak/MyEntitiesRepository.java
@@ -2,7 +2,7 @@
  * WARNING: This application contains serious security vulnerabilities. Run it at your own risk! 
  * It is recommended using a backed-up and sheltered environment (such as a VM with a recent snapshot 
  * and host-only networking). Do not upload this application to any Internet facing servers, 
- * as they will be compromised.
+ * as they will be compromised. OK.
  * 
  * DISCLAIMER: I do not take responsibility for the way in which any one uses this application 
  * (SpringBreakVulnerableApplication). The only purpose of this application is to be a test scenario 
